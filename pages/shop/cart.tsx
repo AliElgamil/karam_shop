4e46c99737a1d52a430a2010ca98bc292e87052e
Ali Elgamel <alielgamil345@gmail.com>
2023-06-14 14:03:36 +0200

    update

--- pages/shop/cart.tsx
+++ pages/shop/cart.tsx
@@ -20,25 +20,29 @@ export default function Cart() {
       </Banner>
       <div className="py-[70px]">
         <div className="container m-auto flex gap-8 grow">
-          <div className="flex flex-wrap flex-col grow gap-8 max-w-[600px] m-auto">
-            <ul className="flex flex-col gap-4 grow">
-              {cartItems.map((cartItem, index) => (
-                <CartItem item={cartItem} key={index} />
-              ))}
-              <li className="flex grow justify-between text-head-color items-baseline">
-                <h2 className="text-[1.5rem] font-bold">Subtotal</h2>
-                <span className="text-[1.2rem] font-bold">${totalPrice}</span>
-              </li>
-            </ul>
-            <div>
-              <Link
-                href={`${SHOP}/checkout`}
-                className="gradient-btn py-3 px-8 rounded-lg capitalize text-white"
-              >
-                checkout
-              </Link>
+          {cartItems.length ? (
+            <div className="flex flex-wrap flex-col grow gap-8 max-w-[600px] m-auto">
+              <ul className="flex flex-col gap-4 grow">
+                {cartItems.map((cartItem, index) => (
+                  <CartItem item={cartItem} key={index} />
+                ))}
+                <li className="flex grow justify-between text-head-color items-baseline">
+                  <h2 className="text-[1.5rem] font-bold">Subtotal</h2>
+                  <span className="text-[1.2rem] font-bold">${totalPrice}</span>
+                </li>
+              </ul>
+              <div>
+                <Link
+                  href={`${SHOP}/checkout`}
+                  className="gradient-btn py-3 px-8 rounded-lg capitalize text-white"
+                >
+                  checkout
+                </Link>
+              </div>
             </div>
-          </div>
+          ) : (
+            <p>No Items</p>
+          )}
         </div>
       </div>
     </div>
